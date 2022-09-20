bc886984cbc7e50ff53e4b663b33f9dfeb8eb2c6
Danila <mr.uprivanov@yandex.ru>
2022-09-20 17:40:37 +0300

    Validation with visual effects for registration form

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { RouteReuseStrategy } from '@angular/router';
+import { map, catchError } from 'rxjs';
 import { UserForRegistrationDto } from 'src/app/data/datatransferbojects/UserForRegistration';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
@@ -9,6 +13,8 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class RegisterComponent implements OnInit {
 
+  isError: boolean = false;
+
   user: UserForRegistrationDto = new UserForRegistrationDto();
 
   constructor(private authentication: AuthenticationService) { }
@@ -17,9 +23,26 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(){
-    this.authentication.registerUser(this.user).subscribe(result => console.log(result));
+      this.authentication.registerUser(this.user).pipe(map((data: any) => {
+        console.log(data);
+        return data;
+      }), 
+       catchError(err => {
+        console.log(err);
+        return [];
+       }));
     return false;
   }
 
+  matchingPassword(password: NgModel, passwordConfirmation: NgModel){
+    if(password.value !== passwordConfirmation.value)
+      return false;
+
+    return true;
+  }
+
+  buttonIsDisabled(){
+
+  }
 
 }
